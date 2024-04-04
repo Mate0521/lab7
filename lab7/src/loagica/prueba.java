4bb8d6af21f9bcbcf8087cd67d6ed7164cfc9277
Mateo Card <110208091+Mate0521@users.noreply.github.com>
2024-04-03 23:08:04 -0500

    fin de lab 7

--- lab7/src/loagica/prueba.java
+++ lab7/src/loagica/prueba.java
@@ -1,14 +1,33 @@
 package loagica;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class prueba {
+    @Test
+    public void testConsignar() {
+        Cuenta cuenta = new Cuenta(1, 1000, "Ahorros");
+        cuenta.consignar(500);
+        assertEquals(1500, cuenta.getSaldo());
+    }
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testRetirar() {
+        Cuenta cuenta = new Cuenta(1, 1000, "Ahorros");
+        boolean resultado = cuenta.retirar(500);
+        assertTrue(resultado);
+        assertEquals(500, cuenta.getSaldo());
+    }
+
+    @Test
+    public void testRetirarSinSaldo() {
+        Cuenta cuenta = new Cuenta(1, 1000, "Ahorros");
+        boolean resultado = cuenta.retirar(2000);
+        assertTrue(!resultado);
+        assertEquals(1000, cuenta.getSaldo());
+    }
+
 
 }
